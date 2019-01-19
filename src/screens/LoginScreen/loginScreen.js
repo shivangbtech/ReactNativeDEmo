46541ffa5b25d00ae96d71d9d7ffaf40e6efdfe3
shivang <shivang.goel@tothenew.com>
2019-01-20 00:10:00 +0530

    create login screen : in progress

---
+++ src/screens/LoginScreen/loginScreen.js
@@ -0,0 +1,44 @@
+import React,  { PureComponent } from 'react';
+import {
+    View,
+    Text,
+    TouchableOpacity,
+    TextInput
+} from 'react-native';
+
+class loginScreen extends PureComponent {
+
+    constructor(props) {
+        super(props);
+        this.state = { text: 'Username' };
+    }
+
+
+    static navigationOptions = {
+        header: null
+    }
+
+
+    render() {
+        const { navigation } = this.props;
+        return (
+            <View style={{flex: 1, backgroundColor: '#952f89'}}>
+                <Text> Igram </Text>
+
+                <TextInput
+                    style={{height: 40, borderColor: '#ac58a0', borderWidth: 1}}
+                    onChangeText={(text) => this.setState({text})}
+                    value={this.state.text}
+                />
+
+                <TouchableOpacity onPress={() => navigation.navigate('Details')}>
+                    <Text>
+                        Go to Details
+                    </Text>
+                </TouchableOpacity>
+            </View>
+        );
+    }
+}
+
+export default homeScreen;
